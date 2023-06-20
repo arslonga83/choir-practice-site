9ec6d7f205c8b4b2112a269369e55ce7f1dd0017
Jeff <107892110+arslonga83@users.noreply.github.com>
2023-06-20 15:10:20 -0700

    adding milliseconds

--- client/src/js/schedule.js
+++ client/src/js/schedule.js
@@ -12,7 +12,7 @@ export async function getScheduleHtml() {
     // these lines skip past dates and limit list to 10 upcoming dates
     // be careful of date format in sheet...I think we will need to include year for this to work
     let date = new Date(Date.parse(item.date))
-    if (date >= new Date(Date.UTC()) && listLength <= 9) {
+    if (date >= (Date.now() + (3600 * 1000 * 24)) && listLength <= 9) {
       scheduleHtml += `
       <li class="list-group-item d-lg-flex  align-items-center justify-content-center text-center">
         <p class="mb-lg-0 mb-md-1">${item.date}</p>
