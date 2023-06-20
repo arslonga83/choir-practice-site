fbb53c8c9887d0780d0ccde7e64a0e897d88e024
Jeff <107892110+arslonga83@users.noreply.github.com>
2023-06-20 14:42:36 -0700

    update upcoming dates logic

--- client/src/js/schedule.js
+++ client/src/js/schedule.js
@@ -1,5 +1,3 @@
-// import { scheduleData } from '../files/data/scheduleData'
-
 
 export async function getScheduleHtml() {
 
@@ -14,7 +12,7 @@ export async function getScheduleHtml() {
     // these lines skip past dates and limit list to 10 upcoming dates
     // be careful of date format in sheet...I think we will need to include year for this to work
     let date = new Date(Date.parse(item.date))
-    if (date >= Date.now() && listLength <= 9) {
+    if (date >= Date.now() + 1 && listLength <= 9) {
       scheduleHtml += `
       <li class="list-group-item d-lg-flex  align-items-center justify-content-center text-center">
         <p class="mb-lg-0 mb-md-1">${item.date}</p>
