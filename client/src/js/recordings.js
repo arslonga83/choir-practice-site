3bb970c37be2b8800a77cb2ad08fbed012f1925b
Jeff <107892110+arslonga83@users.noreply.github.com>
2023-08-02 14:18:51 -0700

    updated code to allow recordings for up to 5 songs on a given date

--- client/src/js/recordings.js
+++ client/src/js/recordings.js
@@ -20,6 +20,9 @@ function formatDate(givenDate) {
     let mixedHtml = ''
     let anthemHtml = ''
     let offertoryHtml = ''
+    let song3Html = ''
+    let song4Html = ''
+    let song5Html = ''
 
     for (let item of recordingsData) {
       // skip dates that have passed
@@ -75,17 +78,52 @@ function formatDate(givenDate) {
       </div>
         `
       }
+
+      if (item['song3']) {
+        song3Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item.song3}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['song3-src']}"/>
+        </audio>
+        </div>
+        `
+      }
+      if (item['song4']) {
+        song4Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item.song4}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['song4-src']}"/>
+        </audio>
+        </div>
+        `
+      }
+      if (item['song5']) {
+        song5Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item.song5}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['song5-src']}"/>
+        </audio>
+        </div>
+        `
+      }
       
-        mixedHtml += ` <div class="card text-center col-lg-5 col-md-12 rounded bg-secondary text-white mt-5" >
-        <div class="card-body">
-          <h1 class="card-title fw-bolder">${formattedDate}</h1>
-          ${item.anthem ? `
-          <p class="card-subtitle py-2 fs-5 fw-bold">${item.anthem}</p> ${anthemHtml}` : `<h3 class="py-3">Anthem TBD</h1>`}
-          ${item.offertory ? ` 
-          <p class="card-subtitle py-3 fs-5 fw-bold">${item.offertory}</p>
-          ${offertoryHtml}` : `<h3 class="py-3">Offertory TBD</h1>`}
-          </div>
-        </div>`
+        mixedHtml += ` 
+          <div class="card text-center col-lg-5 col-md-12 rounded bg-secondary text-white mt-5" >
+            <div class="card-body">
+            <h1 class="card-title fw-bolder">${formattedDate}</h1>
+            ${item.anthem ? `
+            <p class="card-subtitle py-2 fs-5 fw-bold">${item.anthem}</p> ${anthemHtml}` : `<h3 class="py-3">Anthem TBD</h1>`}
+            ${item.offertory ? ` 
+            <p class="card-subtitle py-3 fs-5 fw-bold">${item.offertory}</p>
+            ${offertoryHtml}` : `<h3 class="py-3">Offertory TBD</h1>`}
+            ${item.song3 ? `${song3Html}` : ``}
+            ${item.song4 ? `${song4Html}` : ``}
+            ${item.song5 ? `${song5Html}` : ``}
+            </div>
+          </div>`
       }
     
     mixedSection.innerHTML += mixedHtml 
