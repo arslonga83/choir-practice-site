18552d45ff3c756d465fadda9fc4610a73bf2340
Jeff <107892110+arslonga83@users.noreply.github.com>
2025-11-06 16:03:32 -0800

    add recordings to mission page

---
+++ client/src/dist/assets/mission-f0ee2313.js
@@ -0,0 +1,79 @@
+import"./main-b0e50acb.js";import"./links-1827486a.js";import{i as u}from"./customPlayers-b2c5c59d.js";$();async function $(){const k=await(await fetch("missionTracks")).json(),p=document.getElementById("missionTracks");let l="";for(let t of k){let a="",c="",s="",r="",i="",e="",o="",d="",n="";t["track1-title"]&&(a=`
+        <p class="card-subtitle py-3 fs-5 fw-bold">${t["track1-title"]}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${t["track1-link"]}"/>
+        </audio>
+        </div>
+        `),t["track2-title"]&&(c=`
+        <p class="card-subtitle py-3 fs-5 fw-bold">${t["track2-title"]}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${t["track2-link"]}"/>
+        </audio>
+        </div>
+        `),t["track3-title"]&&(s=`
+        <p class="card-subtitle py-3 fs-5 fw-bold">${t["track3-title"]}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${t["track3-link"]}"/>
+        </audio>
+        </div>
+        `),t["track4-title"]&&(r=`
+        <p class="card-subtitle py-3 fs-5 fw-bold">${t["track4-title"]}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${t["track4-link"]}"/>
+        </audio>
+        </div>
+        `),t["track5-title"]&&(i=`
+        <p class="card-subtitle py-3 fs-5 fw-bold">${t["track5-title"]}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${t["track5-link"]}"/>
+        </audio>
+        </div>
+        `),t["track6-title"]&&(e=`
+        <p class="card-subtitle py-3 fs-5 fw-bold">${t["track6-title"]}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${t["track6-link"]}"/>
+        </audio>
+        </div>
+        `),t["track7-title"]&&(o=`
+        <p class="card-subtitle py-3 fs-5 fw-bold">${t["track7-title"]}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${t["track7-link"]}"/>
+        </audio>
+        </div>
+        `),t["track8-title"]&&(d=`
+        <p class="card-subtitle py-3 fs-5 fw-bold">${t["track8-title"]}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${t["track8-link"]}"/>
+        </audio>
+        </div>
+        `),t["track9-title"]&&(n=`
+        <p class="card-subtitle py-3 fs-5 fw-bold">${t["track9-title"]}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${t["track9-link"]}"/>
+        </audio>
+        </div>
+        `),l+=` 
+          <div class="card text-center col-lg-5 col-md-12 rounded bg-dark text-white mt-5" >
+            <div class="card-body">
+            <h1 class="card-title fw-bolder">${t.title}</h1>
+            ${t["track1-title"]?`${a}`:""}
+            ${t["track2-title"]?`${c}`:""}
+            ${t["track3-title"]?`${s}`:""}
+            ${t["track4-title"]?`${r}`:""}
+            ${t["track5-title"]?`${i}`:""}
+            ${t["track6-title"]?`${e}`:""}
+            ${t["track7-title"]?`${o}`:""}
+            ${t["track8-title"]?`${d}`:""}
+            ${t["track9-title"]?`${n}`:""}
+             
+            </div>
+          </div>`}p.innerHTML+=l,u()}
---
+++ client/src/js/missionTracks.js
@@ -0,0 +1,141 @@
+import { initializePlayers } from './customPlayers'
+
+getMissionTracksHtml()  
+
+export async function getMissionTracksHtml() {
+
+    const response = await fetch('missionTracks')
+    const recordingsData = await response.json()
+
+    const missionTracks = document.getElementById('missionTracks')
+    let missionTracksHtml = ``
+
+    for (let item of recordingsData) {
+      
+     
+      let track1Html = ''
+      let track2Html = ''
+      let track3Html = ''
+      let track4Html = ''
+      let track5Html = ''
+      let track6Html = ''
+      let track7Html = ''
+      let track8Html = ''
+      let track9Html = ''
+
+
+      if (item['track1-title']) {
+        track1Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item['track1-title']}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['track1-link']}"/>
+        </audio>
+        </div>
+        `
+      }
+      if (item['track2-title']) {
+        track2Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item['track2-title']}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['track2-link']}"/>
+        </audio>
+        </div>
+        `
+      }
+      if (item['track3-title']) {
+        track3Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item['track3-title']}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['track3-link']}"/>
+        </audio>
+        </div>
+        `
+      }
+      if (item['track4-title']) {
+        track4Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item['track4-title']}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['track4-link']}"/>
+        </audio>
+        </div>
+        `
+      }
+      if (item['track5-title']) {
+        track5Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item['track5-title']}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['track5-link']}"/>
+        </audio>
+        </div>
+        `
+      }
+      if (item['track6-title']) {
+        track6Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item['track6-title']}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['track6-link']}"/>
+        </audio>
+        </div>
+        `
+      }
+      if (item['track7-title']) {
+        track7Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item['track7-title']}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['track7-link']}"/>
+        </audio>
+        </div>
+        `
+      }
+      if (item['track8-title']) {
+        track8Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item['track8-title']}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['track8-link']}"/>
+        </audio>
+        </div>
+        `
+      }
+      if (item['track9-title']) {
+        track9Html = `
+        <p class="card-subtitle py-3 fs-5 fw-bold">${item['track9-title']}</p>
+        <div class="plyr-container">
+        <audio class="custom-player" controls preload="none">
+          <source src="${item['track9-link']}"/>
+        </audio>
+        </div>
+        `
+      }
+     
+        missionTracksHtml += ` 
+          <div class="card text-center col-lg-5 col-md-12 rounded bg-dark text-white mt-5" >
+            <div class="card-body">
+            <h1 class="card-title fw-bolder">${item.title}</h1>
+            ${item['track1-title'] ? `${track1Html}` : ``}
+            ${item['track2-title'] ? `${track2Html}` : ``}
+            ${item['track3-title'] ? `${track3Html}` : ``}
+            ${item['track4-title'] ? `${track4Html}` : ``}
+            ${item['track5-title'] ? `${track5Html}` : ``}
+            ${item['track6-title'] ? `${track6Html}` : ``}
+            ${item['track7-title'] ? `${track7Html}` : ``}
+            ${item['track8-title'] ? `${track8Html}` : ``}
+            ${item['track9-title'] ? `${track9Html}` : ``}
+             
+            </div>
+          </div>`
+      }
+    
+    missionTracks.innerHTML += missionTracksHtml 
+      
+    initializePlayers()
+  }
+
+
